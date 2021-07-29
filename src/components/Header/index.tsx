6ebb49a43d7800fc74f45ae40ab70ff3b264331d
Camila Mizumoto <camila.yuri.m@gmail.com>
2021-07-29 13:59:55 -0300

    header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
-export default function Header() {
-  // TODO
+import styles from './header.module.scss'
+
+export function Header() {
+  return(
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <nav>
+          <img src="/images/logo.svg" alt="Blog from zero logo" />
+        </nav>
+      </div>
+    </header>
+  );
 }
