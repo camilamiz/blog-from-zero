a7ba94422e337e9931c548f79e59bcbf56cdd806
Camila Mizumoto <camila.yuri.m@gmail.com>
2021-08-02 10:49:25 -0300

    add prismic preview script to load in every page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,7 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript></NextScript>
+          <script async defer src="https://static.cdn.prismic.io/prismic.js?new=true&repo=blog-from-zero"></script>
         </body>
       </Html>
     );
